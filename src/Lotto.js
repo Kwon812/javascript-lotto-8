e55234cb1e66dd6ae0785c7f1cefd0d19b3b0495
Kwon812 <tre723@naver.com>
2025-10-28 16:10:40 +0900

    feat: Lotto class edit

--- src/Lotto.js
+++ src/Lotto.js
@@ -7,12 +7,19 @@ class Lotto {
   }
 
   #validate(numbers) {
+    const isValidNumbers=numbers.every(number=>number>0 && number<46)
     if (numbers.length !== 6) {
       throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+    }
+    if (!isValidNumbers) {
+      throw new Error('[ERROR] 로또 번호는 1이상 45 이하여야 합니다')
     }
   }
 
   // TODO: 추가 기능 구현
+  getNumbers(){
+    return this.#numbers;
+  }
 }
 
 export default Lotto;
---
+++ test.js
@@ -0,0 +1,3 @@
+const numbers=[1,2,3,46,5,6]
+const isValid=numbers.every(number=>number>0 && number<46)
+console.log(isValid)
